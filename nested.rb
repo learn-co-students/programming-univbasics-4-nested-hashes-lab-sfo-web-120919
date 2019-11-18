cd30720e12e03b030fd9ecd8a3a1311cf2991587
Aaron Kim <akaaronkim@gmail.com>
2019-11-18 20:56:16 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -113,9 +113,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+   
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+	    programmer_hash
      
 end
 
@@ -135,5 +137,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	  programmer_hash
 end
